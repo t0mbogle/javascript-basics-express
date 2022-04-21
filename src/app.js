fb3b88a0cca2982695d9b0ebbdd550a3ef765cfb
Thomas <thomas.bogle@outlook.com>
2022-04-21 13:01:52 +0100

    uppercase function added to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { sayHello } = require('./lib/strings');
+const { uppercase } = require('./lib/strings');
 
 const app = express();
 
-app.get('/strings/hello/:strings', (req, res) => {
-  res.status(200).json({ result: sayHello(req.params.strings) });
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).json({ result: sayHello(req.params.string) });
 });
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+// lowercase
+
+// first-characters
+
+// nth character
 
 module.exports = app;
